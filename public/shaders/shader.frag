3197419b3a6cc05440595afd93e6261ba56f5df4
Nick Rodgers <nick.rodgers42@gmail.com>
2023-10-09 22:39:57 -0400

    Update shader render function

--- public/shaders/shader.frag
+++ public/shaders/shader.frag
@@ -7,7 +7,29 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform float u_time;
 
+// vec4 gl_FragCoord - position of the current pixel being rendered
+// vec4 gl_FragColor - output color of the current pixel
 void main() {
-    vec2 st = gl_FragCoord.xy / u_resolution;
-    gl_FragColor = vec4(0.0, abs(sin(u_time)), 1.0 - abs(sin(u_time)), 1.0);
+    // uv is the position of gl_FragCoord in clip space
+    // vec2 uv = gl_FragCoord.xy / u_resolution;
+
+    // // center (0, 0) in the middle of the canvas
+    // uv -= 0.5;
+    // // re-adjust the clip space to be from -1 to 1
+    // uv *= 2.0;
+    // uv = (uv - 0.5) * 2.0;
+    vec2 uv = (gl_FragCoord.xy / u_resolution) * 2.0 - 1.0;
+    // scale width to height so it doesn't distort
+    uv.x *= u_resolution.x / u_resolution.y;
+    // uv = (gl_FragCoord * 2.0 - u_resolution.xy) / u_resolution.y;
+
+    
+    float d = length(uv);
+    d = sin(d * 8.0 + u_time * 4.0) / 8.0;
+    d = abs(d);
+    // d = step (0.05, d);
+    d = smoothstep(0.08, 0.1, d);
+
+
+    gl_FragColor = vec4(0.0, d, d, 1.0);
 }
--- src/ShaderCanvas.tsx
+++ src/ShaderCanvas.tsx
@@ -40,54 +40,60 @@ const ShaderCanvas = (props: ShaderCanvasProps) => {
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
   }
 
-  const setUniforms = (gl: WebGLRenderingContext, program: WebGLProgram, time: number) => {
+  const setUniforms = (gl: WebGLRenderingContext, program: WebGLProgram, time: number): void => {
     setUTime(gl, program, time)
     setUResolution(gl, program)
+    // TODO: setUMouse()
   }
 
-  const render = (time: number) => {
-    if (canvasRef.current !== null) {
-      setCanvasSize(canvasRef.current)
-
-      const gl = canvasRef.current.getContext("webgl")
-      if (gl === null) {
-        throw Error('Could not get canvas context')
-      }
-
-      const program = compileShader(gl)
-
-      const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-      const positionBuffer = gl.createBuffer()
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-      const positions = [
-        -1, -1,
-        -1, 1,
-        1, -1,
-        1, 1,
-        1, -1,
-        -1, 1
-      ]
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-      gl.clearColor(1, 1, 1, 1)
-      gl.clear(gl.COLOR_BUFFER_BIT)
-      gl.useProgram(program)
-      gl.enableVertexAttribArray(positionAttributeLocation)
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-      setUniforms(gl, program, time)
-
-      const size = 2
-      const type = gl.FLOAT
-      const normalize = false
-      const stride = 0
-      const offset = 0
-      gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
-      const primitiveType = gl.TRIANGLES
-      const count = 6
-      gl.drawArrays(primitiveType, offset, count)
+  const render = (time: number): void => {
+    if (canvasRef.current === null) {
+      return
     }
+
+    setCanvasSize(canvasRef.current)
+
+    const gl = canvasRef.current.getContext("webgl")
+    if (gl === null) {
+      throw Error('Could not get canvas context')
+    }
+
+    const program = compileShader(gl)
+
+    const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
+    const positionBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+    // We have to draw two traingles to cover the canvas
+    const positions = [
+      // Triangle 1
+      -1, -1,
+      -1, 1,
+      1, -1,
+      // Triangle 2
+      1, 1,
+      1, -1,
+      -1, 1
+    ]
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+    gl.clearColor(1, 1, 1, 1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.useProgram(program)
+    gl.enableVertexAttribArray(positionAttributeLocation)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+    setUniforms(gl, program, time)
+
+    const size = 2
+    const type = gl.FLOAT
+    const normalize = false
+    const stride = 0
+    const offset = 0
+    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
+    const primitiveType = gl.TRIANGLES
+    const count = 6
+    gl.drawArrays(primitiveType, offset, count)
   }
 
   useEffect(() => {
